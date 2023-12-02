bec0c2cf162482392a878d6f2bd906b6be5aa78b
davi-aguiar-vieira <davideaguiarvieira@gmail.com>
2023-12-02 20:29:49 -0300

    Atualização_cadastro

--- Cadastro.py
+++ Cadastro.py
@@ -9,7 +9,7 @@ clock = pygame.time.Clock()
 
 # Fonte
 main_font = pygame.font.Font('Fontes/Bahnschrift.ttf', 15)
-other_font = pygame.font.Font('Fontes/Bahnschrift.ttf', 15)
+input_font = pygame.font.Font('Fontes/Bahnschrift.ttf', 20)
 title_font_bold = pygame.font.Font('Fontes/Karla.ttf', 25)
 title_font_bold.set_bold(True)
 
@@ -26,6 +26,7 @@ background_surface = pygame.image.load('Imagens/Cadastro.jpg')
 arrow = pygame.image.load('Imagens/Botão seta.png')
 arrow = pygame.transform.scale(arrow, (60, 60))
 arrow_rect = arrow.get_rect(topleft=(118, 560))
+arrow_hovered = False
 
 # Criar o objeto retângulo
 rectangle = pygame.Surface((305, 755))
@@ -62,6 +63,21 @@ username_box = pygame.Rect(30, 210, input_box_width, input_box_height)
 password_box = pygame.Rect(30, 310, input_box_width, input_box_height)
 confirm_password_box = pygame.Rect(30, 410, input_box_width, input_box_height)
 
+# Variáveis para armazenar o texto dos campos
+username_text = ''
+password_text = ''
+confirm_password_text = ''
+
+# Função para adicionar texto aos campos
+def draw_text_in_box(surface, text, color, rect, font):
+    text_surface = font.render(text, True, color)
+    text_rect = text_surface.get_rect(topleft=(rect.x + 5, rect.y + 5))
+    if text_rect.width > rect.width - 10:
+        text = text[:len(text)-1]
+        text_surface = font.render(text, True, color)
+        text_rect = text_surface.get_rect(topleft=(rect.x + 5, rect.y + 5))
+    surface.blit(text_surface, text_rect)
+
 while True:
     mouse_x, mouse_y = pygame.mouse.get_pos()
     
@@ -69,12 +85,46 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             if button_rect_fazer_login.collidepoint(event.pos):
                 pygame.quit()
                 login_script = os.path.join(os.path.dirname(__file__), "Login.py")
                 os.system(f"python {login_script}")
                 sys.exit()
+
+            elif arrow_rect.collidepoint(event.pos):
+                if username_text and password_text and confirm_password_text:
+                    if password_text == confirm_password_text:
+                        # Salvar dados do jogador em arquivo TXT
+                        player_data = f"{username_text},{password_text}\n"  # Formato: nome de usuário, senha
+
+                        with open('dados_jogadores.txt', 'a') as file:  # 'a' para adicionar ao arquivo
+                            file.write(player_data)
+
+                        print("Dados do jogador salvos com sucesso!")
+                    else:
+                        print("Senhas não coincidem!")
+                else:
+                    print("Preencha todos os campos!")
+
+        # Captura a entrada do teclado para os campos de texto
+        if event.type == pygame.KEYDOWN:
+            if username_box.collidepoint(mouse_x, mouse_y):
+                if event.key == pygame.K_BACKSPACE:
+                    username_text = username_text[:-1]
+                else:
+                    username_text += event.unicode
+            elif password_box.collidepoint(mouse_x, mouse_y):
+                if event.key == pygame.K_BACKSPACE:
+                    password_text = password_text[:-1]
+                else:
+                    password_text += event.unicode
+            elif confirm_password_box.collidepoint(mouse_x, mouse_y):
+                if event.key == pygame.K_BACKSPACE:
+                    confirm_password_text = confirm_password_text[:-1]
+                else:
+                    confirm_password_text += event.unicode
 
     # Desenha a imagem de fundo
     screen.blit(background_surface, (100, -50))
@@ -94,7 +144,7 @@ while True:
     text_3 = text3.get_rect()
     text_3.topleft = (30, 280)
     
-    # Posições de text3 no retângulo
+    # Posições de text4 no retângulo
     text_4 = text4.get_rect()
     text_4.topleft = (30, 380)
     
@@ -131,6 +181,20 @@ while True:
 
     # Desenha o botão seta
     screen.blit(arrow, arrow_rect.topleft)
+
+    # Verifica se o mouse está sobre o botão seta
+    arrow_hovered = arrow_rect.collidepoint(mouse_x, mouse_y)
+
+    # Desenha o botão seta com borda diferente se o mouse estiver sobre ele
+    if arrow_hovered:
+        pygame.draw.rect(screen, white_color, arrow_rect, 2)
+    else:
+        pygame.draw.rect(screen, black_color, arrow_rect, 2)
+
+    # Desenha o texto nos campos de entrada
+    draw_text_in_box(screen, username_text, white_color, username_box, main_font)
+    draw_text_in_box(screen, '*' * len(password_text), white_color, password_box, main_font)  # Para senha, exibe asteriscos
+    draw_text_in_box(screen, '*' * len(confirm_password_text), white_color, confirm_password_box, main_font)  # Confirmar senha também exibe asteriscos
 
     pygame.display.update()
     clock.tick(60)
